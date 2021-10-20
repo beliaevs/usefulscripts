f5b10f40d24b0802261a77539cae8714100a4b6f
Alexander Beliaev <beliaevs@gmail.com>
2021-10-20 19:08:25 +0300

    python script for generating arithmetic exercises

---
+++ generate_task.py
@@ -0,0 +1,35 @@
+import random
+
+def generate_task():
+    operations = ['+', '-']
+    MaxResult = 100
+    a = random.randrange(1, 99)
+    sign = random.choice(operations)
+    if sign == '-':
+        b = random.randrange(0, a)
+    else:
+        b = random.randrange(0, MaxResult - a)
+    return (a, sign, b)
+
+def task2equation(task):
+    return str(task[0]) + str(task[1]) + str(task[2]) + '&='
+
+def main():
+    columns = 4
+    rows = 17
+    myseed = 'gsfahegdgsgf'
+    random.seed(myseed)
+
+    headers = [r'\documentclass[12pt, a4paper]{article}', r'\usepackage{amsmath}',  r'\pagestyle{empty}', r'\begin{document}', r'\begin{align*}']
+    footers = [r'\end{align*}', r'\end{document}']
+
+    equations = []
+    for i in range(rows):
+        equation_row = ' & '.join([task2equation(generate_task()) for j in range(columns)])
+        equations.append(equation_row)
+
+    equations_string = '\\\\\n'.join(equations)
+    print('\n'.join(headers) + equations_string + '\n'.join(footers))
+
+if __name__=='__main__':
+    main()
