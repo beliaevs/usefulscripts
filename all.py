ffb92cd6aaa6d15697777db5e15be9a78f6f49e4
Alexander Beliaev <beliaevs@gmail.com>
2022-04-27 11:20:45 +0300

    fix python3 all.py error

--- all.py
+++ all.py
@@ -5,6 +5,10 @@ from pathlib import Path
 
 def main():
     forward_args = sys.argv[1:]
+    if not forward_args:
+        print('empty command')
+        return
+
     p = Path('.')
     for d in p.glob('[a-zA-Z]*'):
         if d.is_dir():
